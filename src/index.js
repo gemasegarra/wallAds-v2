4026db2e10549b89a85e1515509e42bc715eb436
gemasegarra <gema.segb@gmail.com>
2020-05-30 15:52:54 +0200

    import from store folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducer from './Reducers/reducers';
+import reducer from './Store/reducers';
 
 const store = createStore(reducer, {
   username: localStorage.username,
